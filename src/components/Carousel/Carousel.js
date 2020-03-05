fcd625a5188f2b027855be7a6276bcd08752fd92
Enrique Montemayor <eemontemayor@live.com>
2020-03-05 16:02:03 -0600

    circle clicker almost working

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,102 +1,8 @@
 import React, {useEffect} from "react";
 import './Carousel.css'
-export default class Carousel extends React.Component{
-  state = {
-    blurb1:  'Critical Thinker For Hire',
-    blurb2: 'Sentient Bag of StarDust',
-    pairBlurbs:[]
-}
-
-
-// Landing page on hover.... the oppo side the user is hovering on will reveal the background image underneath
-  componentDidMount() {// transfer blurb array from props and use circle as a button that will change the quotes on click/hover
-    const blurbArray = ['Whimsical Wordsmith', '1 Blah blah blah', '2 blah blah blah', '3 blah blah blah', 'Lover of Tacos', 'Endorphin Enthusiast']
-    const pairedBlurbs = this.pairBlurbs(this.shuffleBlurbs(blurbArray))
-    this.setState({
-      blurbPairs: pairedBlurbs
-    })
-
-  }
-  shuffleBlurbs = (arr) => {
-    let blurbArr = arr
-    let shuffle = function (array) {
-
-      var currentIndex = array.length;
-      var temporaryValue, randomIndex;
-    
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-    
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
-    
-      return array;
-    
-    };
-
-   return shuffle(blurbArr) 
-  }
-
-  pairBlurbs = (arr) => {
-    // const firstArr = ['Critical Thinker For Hire', 'Sentient Bag of StarDust']
-    const lastArr= [' ','Ran out of ideas']
-    
-    let input = arr 
-
-    let combArr = [
-      // firstArr,
-      [input[0], input[1]],
-      [input[2], input[3]],
-      // [input[4], input[5]],
-     
-      lastArr
-    ]
-    // console.log(combArr)
-    return combArr
-  
-  }
-  renderBlurbs = (delay=5000) => {
-
-   
-    // for (let i = 0; i < finalArr.length; i++){
-    //   setTimeout(function () {
-     
-    //   }, delay);
-    //   this.setBlurbs(finalArr[i])
-    // }
-
-  //   this.setState({
-  //    blurb1:'hey you'
-  //   }, () => {
-  //     setTimeout(function () {
-  //       // this.setState({ blurb1: finalArr[4] })
-  //       console.log('aqui')
-  //     }, delay)
-  //  })
-
-  }
-
-  setBlurbs = (arr) => {
-    this.setState({
-      blurb1: arr[0],
-      blurb2:arr[1]
-    }, () => {
-        console.log(this.state)
-    })
-  }
-
-  render() {
-   this.renderBlurbs()
-    const blurb1 = this.state.blurb1
-      // || 'Critical Thinker For Hire'
-    const blurb2 = this.state.blurb2
-      // || 'Sentient Bag of Startdust'
+export default function Carousel(props){
+  let blurb1 = props.blurb1
+  let blurb2 = props.blurb2
     return( <>
       <p className='blurb1' >{blurb1} </p>
       <p className='blurb2' >{blurb2} </p>
@@ -104,4 +10,4 @@ export default class Carousel extends React.Component{
       </>)
     }
   
-}
+
--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -6,24 +6,98 @@ export default class LandingPage extends React.Component {
 
 
     state = {
-        blurbs : ['Whimsical Wordsmith','1 Blah blah blah','2 blah blah blah']
+      blurb1: '',
+      blurb2: '',
+      pairedBlurbs:[]
     }
 
 
-    componentDidMount() {
-        
+  
+// Landing page on hover.... the oppo side the user is hovering on will reveal the background image underneath
+componentDidMount() {// transfer blurb array from props and use circle as a button that will change the quotes on click/hover
+  const blurbArray = ['Whimsical Wordsmith', '1 Blah blah blah', '2 blah blah blah', '3 blah blah blah', 'Lover of Tacos', 'Endorphin Enthusiast']
+  const pairedBlurbs = this.pairBlurbs(this.shuffleBlurbs(blurbArray))
+  this.setState({
+    blurb1: pairedBlurbs[0][0],
+    blurb2:pairedBlurbs[0][1],
+    blurbPairs: pairedBlurbs
+  })
+
+}
+shuffleBlurbs = (arr) => {
+  let blurbArr = arr
+  let shuffle = function (array) {
+
+    var currentIndex = array.length;
+    var temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
+  
+    return array;
+  
+  };
+
+ return shuffle(blurbArr) 
+}
+
+pairBlurbs = (arr) => {
+  const firstArr = ['Critical Thinker For Hire', 'Sentient Bag of StarDust']
+  const lastArr= [' blah blha blah bbbla ','Ran out of ideas']
+  
+  let input = arr 
+
+  let combArr = [
+    firstArr,
+    [input[0], input[1]],
+    [input[2], input[3]],
+    [input[4], input[5]],
+    // [input[6], input[7]],
+    lastArr
+  ]
+  // console.log(combArr)
+  return combArr
+
+}
+  
+  handleClick = () => {
+    // const lastArr= [' blah blha blah bbbla ','Ran out of ideas']
+    let arr = this.state.blurbPairs
+    let oldBlurbs= arr.shift()
+    let newBlurb1 = arr[0][0]
+    let newBlurb2 = arr[0][1]
+    arr.push([oldBlurbs])
+    
+  this.setState({
+    blurb1: newBlurb1,
+    blurb2: newBlurb2,
+    blurbPairs:arr
+  }, () => {
+      console.log(this.state)
+  })
+  }
+
+ 
   render() {
     return (
       <div className="LandingPage">
         <div className='circle-wrapper'>
 
-            <section className="circle">
+            <section className="circle" onClick={this.handleClick}>
                 <div id='circ-content'>
 
           <h2><span className='firstName'>Quique</span>  <br/><span className='lastName'>Montemayor</span></h2>
                 <h1>Full Stack Developer</h1>
-                <Carousel blurbs={this.state.blurbs}/>
+                <Carousel blurb1={this.state.blurb1} blurb2={this.state.blurb2}/>
                 </div>
         </section>
         </div>
