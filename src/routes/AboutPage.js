ec1e0c831405599e1e942a24af3bebba4f027fb4
Enrique Montemayor <eemontemayor@live.com>
2020-03-09 13:58:13 -0500

    x

--- src/routes/AboutPage.js
+++ src/routes/AboutPage.js
@@ -14,7 +14,7 @@ export default function AboutPage(props) {
                     <Split className='white'id='about-mob-txt'  >
                 <section className='about-text-container'>
                     <p className='about-text'>
-                        I am a full-stack developer based in Austin, Tx. Before enrolling in Thinkful's Engineering Immersion Program this past January, I was teaching philosophy at the university level. Now, I am excited about this new phase in my life and eager to continue building on what I have already learned.
+                        I am a full-stack developer based in Austin, Tx. Before enrolling in Thinkful's Engineering Immersion Program, I was teaching philosophy at the university level. Now, I am excited about this new phase in my life and eager to continue building on what I have already learned.
                     </p>
                     <p className='about-text'>
                     I am pleasantly surprised by how much I enjoy solving the puzzles presented in this particular professional paradigm and I am encouraged by the supportive on-line community programmers provide for each other. I look forward to working with a team of fellow programmers and hope I can contribute some good to the world with my new set of skills.
